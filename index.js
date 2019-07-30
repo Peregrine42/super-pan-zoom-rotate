b8126e7b12d69854933c7bb741fa3bbe729c3bfb
Duncan Fermor <duncan_fermor@fastmail.com>
2019-07-30 06:12:23 +0100

    complete the camera

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 window.addEventListener("load", function() {
-  screenMatrix = TransformationMatrix.translate(250, 250);
+  svg = document.getElementById("global");
+  cameraSVG = document.getElementById("camera");
+
+  // Global setup
+
+  screenMatrix = TransformationMatrix.translate(
+    svg.getAttribute("width") / 2,
+    svg.getAttribute("height") / 2
+  );
   screenCenter = TransformationMatrix.applyToPoint(screenMatrix, {
     x: 0,
     y: 0
   });
 
-  svg = document.getElementById("test");
   xAxisSVG = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
   xAxisSVG.setAttribute("x1", screenCenter.x);
@@ -30,39 +37,91 @@ window.addEventListener("load", function() {
 
   svg.appendChild(yAxisSVG);
 
+  // Camera setup
+
+  screenMatrix = TransformationMatrix.translate(
+    cameraSVG.getAttribute("width") / 2,
+    cameraSVG.getAttribute("height") / 2
+  );
+  screenCenter = TransformationMatrix.applyToPoint(screenMatrix, {
+    x: 0,
+    y: 0
+  });
+
+  xAxisSVG = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+  xAxisSVG.setAttribute("x1", screenCenter.x);
+  xAxisSVG.setAttribute("y1", screenCenter.y);
+  xAxisSVG.setAttribute("x2", screenCenter.x + 100);
+  xAxisSVG.setAttribute("y2", screenCenter.y);
+  xAxisSVG.setAttribute("stroke-width", "1px");
+  xAxisSVG.setAttribute("stroke", "black");
+  xAxisSVG.setAttribute("marker-end", "url(#arrow)");
+
+  cameraSVG.appendChild(xAxisSVG);
+
+  yAxisSVG = document.createElementNS("http://www.w3.org/2000/svg", "line");
+
+  yAxisSVG.setAttribute("x1", screenCenter.x);
+  yAxisSVG.setAttribute("y1", screenCenter.y);
+  yAxisSVG.setAttribute("x2", screenCenter.x);
+  yAxisSVG.setAttribute("y2", screenCenter.y + 100);
+  yAxisSVG.setAttribute("stroke-width", "1px");
+  yAxisSVG.setAttribute("stroke", "black");
+  yAxisSVG.setAttribute("marker-end", "url(#arrow)");
+
+  cameraSVG.appendChild(yAxisSVG);
+
   list = {
-    x: 50,
-    y: 60,
+    x: 0, // camera coordinates (these stay at 0)
+    y: 0,
     angle: 0,
+    angleCenterX: 0,
+    angleCenterY: 0,
     scale: 1,
     svg: document.createElementNS("http://www.w3.org/2000/svg", "line"),
+    cameraSVG: document.createElementNS("http://www.w3.org/2000/svg", "line"),
     child: {
-      x: 100,
-      y: 120,
+      x: 50, // origin coordinates
+      y: 60,
       angle: 0,
+      angleCenterX: 0,
+      angleCenterY: 0,
       scale: 1,
       svg: document.createElementNS("http://www.w3.org/2000/svg", "line"),
+      cameraSVG: document.createElementNS("http://www.w3.org/2000/svg", "line"),
       child: {
-        x: 60,
-        y: 30,
+        x: 30, // global coordinates
+        y: 15,
         child: null,
         angle: 0,
+        angleCenterX: 0,
+        angleCenterY: 0,
         scale: 1,
-        svg: document.createElementNS("http://www.w3.org/2000/svg", "line")
+        svg: document.createElementNS("http://www.w3.org/2000/svg", "line"),
+        cameraSVG: document.createElementNS(
+          "http://www.w3.org/2000/svg",
+          "line"
+        )
       }
     }
   };
 
-  svg.appendChild(list.svg);
-  svg.appendChild(list.child.svg);
-  svg.appendChild(list.child.child.svg);
+  camera = list;
+  origin = camera.child;
+  point = origin.child;
 
-  selected = list;
+  svg.appendChild(origin.svg);
+  svg.appendChild(point.svg);
 
-  function draw(node, originMatrix) {
+  // cameraSVG.appendChild(camera.cameraSVG);
+  // cameraSVG.appendChild(origin.cameraSVG);
+  cameraSVG.appendChild(point.cameraSVG);
+
+  function draw(node, originMatrix, attribute) {
     if (node == null) return;
 
-    nodeSVG = node.svg;
+    nodeSVG = node[attribute];
 
     nodeSVG.setAttribute("x1", 0);
     nodeSVG.setAttribute("y1", 0);
@@ -70,52 +129,59 @@ window.addEventListener("load", function() {
     nodeSVG.setAttribute("y2", node.y);
     nodeSVG.setAttribute("transform", TransformationMatrix.toCSS(originMatrix));
     nodeSVG.setAttribute("stroke-width", "1px");
-    nodeSVG.setAttribute("stroke", node === selected ? "orange" : "grey");
+    nodeSVG.setAttribute("stroke", "black");
+    nodeSVG.setAttribute("marker-start", "url(#dot)");
     nodeSVG.setAttribute("marker-end", "url(#dot)");
 
     newMatrix = TransformationMatrix.compose(
       originMatrix,
       TransformationMatrix.translate(node.x, node.y),
       TransformationMatrix.scale(node.scale),
-      TransformationMatrix.rotateDEG(node.angle),
+      TransformationMatrix.rotateDEG(
+        node.angle,
+        node.angleCenterX,
+        node.angleCenterY
+      )
     );
-    draw(node.child, newMatrix);
+    draw(node.child, newMatrix, attribute);
   }
 
-  draw(list, screenMatrix);
+  draw(point, screenMatrix, "svg");
+  draw(camera, screenMatrix, "cameraSVG");
 
   window.addEventListener("keyup", function(event) {
-    if (event.key === "n") {
-      if (selected === null) {
-        selected = list;
-      } else {
-        selected = selected.child;
-      }
-      draw(list, screenMatrix);
-    } else if (event.key === "ArrowLeft") {
-      selected.x -= 5;
-      draw(list, screenMatrix);
+    if (event.key === "ArrowLeft") {
+      origin.x += 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "ArrowRight") {
-      selected.x += 5;
-      draw(list, screenMatrix);
+      origin.x -= 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "ArrowUp") {
-      selected.y += 5;
-      draw(list, screenMatrix);
+      origin.y -= 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "ArrowDown") {
-      selected.y -= 5;
-      draw(list, screenMatrix);
+      origin.y += 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "[") {
-      selected.angle += 5;
-      draw(list, screenMatrix);
+      camera.angle -= 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "]") {
-      selected.angle -= 5;
-      draw(list, screenMatrix);
+      camera.angle += 5;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "=") {
-      selected.scale *= 1.1;
-      draw(list, screenMatrix);
+      camera.scale *= 1.1;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     } else if (event.key === "-") {
-      selected.scale *= 0.9;
-      draw(list, screenMatrix);
+      camera.scale *= 0.9;
+      draw(point, screenMatrix, "svg");
+      draw(camera, screenMatrix, "cameraSVG");
     }
   });
 });
